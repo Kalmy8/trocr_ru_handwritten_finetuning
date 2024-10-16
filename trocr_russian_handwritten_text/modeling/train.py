ec2bd9a63b9ddf8c794481e19b9f0edddb18d79e
Kalmy8 <kalmykovalexey01@gmail.com>
2024-10-16 11:28:04 +0500

    feat: training process is implemented inside the train.py file

--- trocr_russian_handwritten_text/modeling/train.py
+++ trocr_russian_handwritten_text/modeling/train.py
@@ -1,3 +1,4 @@
+import pickle
 from pathlib import Path
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 from PIL import Image
@@ -5,37 +6,61 @@ import requests
 import typer
 from loguru import logger
 from tqdm import tqdm
+from transformers import Seq2SeqTrainer, Seq2SeqTrainingArguments
 
+from evaluate import load
+from transformers import default_data_collator
+from trocr_russian_handwritten_text.dataset import HandwrittingDataset
 from trocr_russian_handwritten_text.config import MODELS_DIR, PROCESSED_DATA_DIR
+from models.models import model, processor
+cer_metric = load("cer")
 
-app = typer.Typer()
+def compute_cer(pred):
+    labels_ids = pred.label_ids
+    pred_ids = pred.predictions
 
+    pred_str = processor.batch_decode(pred_ids, skip_special_tokens=True)
+    labels_ids[labels_ids == -100] = processor.tokenizer.pad_token_id
+    label_str = processor.batch_decode(labels_ids, skip_special_tokens=True)
+    cer = cer_metric.compute(predictions=pred_str, references=label_str)
+    return {"cer": cer}
 
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    labels_path: Path = PROCESSED_DATA_DIR / "labels.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Training some model...")
-    for i in tqdm(range(10), total=10):
+def main():
+    logger.info("Training model...")
 
-               #image_source_here
-        image = Image.open().convert("RGB")
+    # Loading the datasets
+    with open(PROCESSED_DATA_DIR / "train_dataset.pkl", "rb") as f:
+        train_dataset = pickle.load(f)
 
-        processor = TrOCRProcessor.from_pretrained('microsoft/trocr-small-handwritten')
-        model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-small-handwritten')
-        pixel_values = processor(images=image, return_tensors="pt").pixel_values
+    with open(PROCESSED_DATA_DIR / "eval_dataset.pkl", "rb") as f:
+        eval_dataset = pickle.load(f)
 
-        generated_ids = model.generate(pixel_values)
-        generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    training_args = Seq2SeqTrainingArguments(
+        predict_with_generate=True,
+        eval_strategy="steps",
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
+        fp16=True,
+        output_dir=MODELS_DIR,
+        logging_steps=2,
+        save_steps=1000,
+        eval_steps=200,
+    )
 
+    # instantiate trainer
+    trainer = Seq2SeqTrainer(
+        model=model,
+        tokenizer=processor.image_processor,
+        args=training_args,
+        compute_metrics=compute_cer,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        data_collator=default_data_collator,
+
+    )
+    trainer.train()
     logger.success("Modeling training complete.")
-    # -----------------------------------------
 
 
 if __name__ == "__main__":
-    app()
+    main()
