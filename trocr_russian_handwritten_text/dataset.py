df5941e7ecbd446c9af19ca82205333343700e24
Kalmy8 <kalmykovalexey01@gmail.com>
2024-10-16 10:12:45 +0500

    feat: completed dataset.py file so the raw data now translates to the processed one and is stored under PROCESSED_DATA_DIR path specified inside config.py

--- trocr_russian_handwritten_text/dataset.py
+++ trocr_russian_handwritten_text/dataset.py
@@ -1,69 +1,73 @@
-from pathlib import Path
-from sklearn.model_selection import train_test_split
+import pickle
+
+import pandas as pd
 import torch
-import typer
+from PIL import Image
 from loguru import logger
-from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset
 from transformers import TrOCRProcessor
 
 from trocr_russian_handwritten_text.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
-app = typer.Typer()
+RANDOM_STATE = 42
+class HandwrittingDataset(Dataset):
+    def __init__(self, image_dir, df, processor, max_target_length=128):
+        self.image_dir = image_dir
+        self.df = df
+        self.processor = processor
+        self.max_target_length = max_target_length
 
+    def __len__(self):
+        return len(self.df)
 
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    # ----------------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Processing dataset...")
-    train_images, train_labels = 0,0
-    text_images, text_labels = 0, 0
+    def __getitem__(self, idx):
+        # get file name + text
+        file_name = self.df['file_name'][idx]
+        text = self.df['text'][idx]
+        # prepare image (i.e. resize + normalize)
+        image = Image.open(self.image_dir + file_name).convert("RGB")
+        pixel_values = self.processor(image, return_tensors="pt").pixel_values
+        # add labels (input_ids) by encoding the text
+        labels = self.processor.tokenizer(text,
+                                          padding="max_length",
+                                          max_length=self.max_target_length).input_ids
+        # important: make sure that PAD tokens are ignored by the loss function
+        labels = [label if label != self.processor.tokenizer.pad_token_id else -100 for label in labels]
 
-    train_texts, val_texts, train_labels, val_labels = train_test_split(train_images, train_labels, test_size=.2)
+        encoding = {"pixel_values": pixel_values.squeeze(), "labels": torch.tensor(labels)}
+        return encoding
 
+def main():
+    logger.info("Processing raw data...")
+
+    df_train = pd.read_csv(RAW_DATA_DIR / "train_labels.txt", header=None, names=['file_name', 'text'], sep=',', quotechar='"', on_bad_lines='skip')
+    df_test = pd.read_csv(RAW_DATA_DIR / "test_labels.txt", header=None, names=['file_name', 'text'], sep=',', quotechar='"', on_bad_lines='skip')
+    df_train, df_val = train_test_split(df_train, test_size=0.2, random_state=RANDOM_STATE)
     processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
 
-    from PIL import Image
+    # Constructing pytorch datasets
+    train_dataset = HandwrittingDataset(image_dir = RAW_DATA_DIR / 'train',
+                               df=df_train,
+                               processor=processor)
+    eval_dataset = HandwrittingDataset(image_dir=RAW_DATA_DIR / 'train',
+                              df=df_val,
+                              processor=processor)
+    test_dataset = HandwrittingDataset(image_dir=RAW_DATA_DIR / 'test',
+                              df=df_test,
+                              processor=processor)
 
-    class HandwrittenDataset(Dataset):
-        def __init__(self, root_dir, df, processor, max_target_length=128):
-            self.root_dir = root_dir
-            self.df = df
-            self.processor = processor
-            self.max_target_length = max_target_length
+    # Saving the datasets
+    with open(PROCESSED_DATA_DIR / "train_dataset.pkl", "wb") as f:
+        pickle.dump(train_dataset, f)
 
-        def __len__(self):
-            return len(self.df)
+    with open(PROCESSED_DATA_DIR / "eval_dataset.pkl", "wb") as f:
+        pickle.dump(eval_dataset, f)
 
-        def __getitem__(self, idx):
-            # get file name + text
-            file_name = self.df['file_name'][idx]
-            text = self.df['text'][idx]
-            # prepare image (i.e. resize + normalize)
-            image = Image.open(self.root_dir + file_name).convert("RGB")
-            pixel_values = self.processor(image, return_tensors="pt").pixel_values
-            # add labels (input_ids) by encoding the text
-            labels = self.processor.tokenizer(text,
-                                              padding="max_length",
-                                              max_length=self.max_target_length).input_ids
-            # important: make sure that PAD tokens are ignored by the loss function
-            labels = [label if label != self.processor.tokenizer.pad_token_id else -100 for label in labels]
+    with open(PROCESSED_DATA_DIR / "test_dataset.pkl", "wb") as f:
+        pickle.dump(test_dataset, f)
 
-            encoding = {"pixel_values": pixel_values.squeeze(), "labels": torch.tensor(labels)}
-            return encoding
-
-    train_dataset = HandwritingsDataset(train_encodings, train_labels)
-    val_dataset = HandwritingsDataset(val_encodings, val_labels)
-    test_dataset = HandwritingsDataset(test_encodings, test_labels)
-
-
-    logger.success("Processing dataset complete.")
-    # -----------------------------------------
-
+    logger.success("Processing raw data complete.")
 
 if __name__ == "__main__":
-    app()
+    main()
